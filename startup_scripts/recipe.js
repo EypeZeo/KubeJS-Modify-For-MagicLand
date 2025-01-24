1ca49b0c48daee92ce39fb5ec8134b91d8e0dd72
EypeZeo Preload <61043370+EypeZeo@users.noreply.github.com>
2025-01-24 15:12:45 +0800

    Added 3 additional slots
    Added 3 extra slots to the Void Miner to show different programmable circuit board configurations required for recipes at different voltage levels

--- startup_scripts/recipe.js
+++ startup_scripts/recipe.js
@@ -4,7 +4,7 @@ GTCEuStartupEvents.registry("gtceu:recipe_type", event => {
     event.create("void_miner")
         .setEUIO("in")
         .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
-        .setMaxIOSize(1, 4, 1, 1)
+        .setMaxIOSize(4, 4, 1, 1)
         .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.MINER)
 
