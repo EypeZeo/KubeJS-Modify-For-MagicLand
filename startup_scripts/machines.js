baa321ba1eb89a232ac8eaccf810c7f35dc85e1e
EypeZeo Preload <61043370+EypeZeo@users.noreply.github.com>
2025-01-24 23:37:53 +0800

    Added 2 more machines
    Added Quantum Energy Compressor and Quantum Energy Decompressor (unfinished) to compress/decompress energy into physical objects and store them.

--- startup_scripts/machines.js
+++ startup_scripts/machines.js
@@ -1,7 +1,7 @@
 // priority: 1
 GTCEuStartupEvents.registry("gtceu:machine", event => {
 
-    event.create("void_miner", "multiblock")
+    event.create("void_miner", "multiblock")  //虚空采矿机
         .rotationState(RotationState.NON_Y_AXIS)
         .allowExtendedFacing(false)
         .recipeType("void_miner")
@@ -23,5 +23,71 @@ GTCEuStartupEvents.registry("gtceu:machine", event => {
                 .where("#", Predicates.any())
                 .build())
         .workableCasingRenderer("gtceu:block/casings/solid/machine_casing_stable_titanium", "gtceu:block/multiblock/large_miner")
+
+    event.create("quantum_energy_compressor", "multiblock") //量子能量压缩机
+        .rotationState(RotationState.NON_Y_AXIS)
+        .allowExtendedFacing(false)
+        .recipeType("quantum_energy_compressor")
+        .tooltips(Component.translatable("gtceu.machine.perfect_oc"))
+        .tooltips(Component.translatable("gtceu.machine.available_recipe_map_1.tooltip",
+            Component.translatable("gtceu.quantum_energy_compressor"),
+            Component.translatable("gtceu.quantum_energy_compressor.description")))
+        .recipeModifier(GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.PERFECT_OVERCLOCK))
+        .appearanceBlock(GTBlocks.CASING_TUNGSTENSTEEL_ROBUST)
+        .pattern((definition) =>
+            FactoryBlockPattern.start()
+                .aisle("CCCCCCCCCGGG", "XXXXXXXXXGGG", "XXXXXXXXXGGG", "CCCCCCCCCGGG", "CCCCCCCCCGGG", "CCCCCCCCCGGG")
+                .aisle("CCCCCCCCCGGG", "XE#E#E#E#BDB", "XD#D#D#D#BDB", "XE#E#E#E#BDB", "XD#D#D#D#BDB", "CCCCCCCCCGGG")
+                .aisle("CCCCCCCCCGGG", "X#E#E#E#EBDB", "X#D#D#D#DBDB", "X#E#E#E#EBDB", "X#D#D#D#DBDB", "CCCCCCCCCGGG")
+                .aisle("CCCCCCCCCGGG", "XAAAAAAAABDB", "XAAAAAAAABDB", "XAAAAAAAABDB", "XAAAAAAAABDB", "CCCCCCCCCGGG")
+                .aisle("CCCCCCCCCGGG", "X#E#E#E#EBDB", "X#D#D#D#DBDB", "X#E#E#E#EBDB", "X#D#D#D#DBDB", "CCCCCCCCCGGG")
+                .aisle("CCCCCCCCCGGG", "XE#E#E#E#BDB", "XD#D#D#D#BDB", "XE#E#E#E#BDB", "XD#D#D#D#BDB", "CCCCCCCCCGGG")
+                .aisle("CCCCCCCCCGGG", "CSCCCCCCCGGG", "CCCCCCCCCGGG", "CCCCCCCCCGGG", "CCCCCCCCCGGG", "CCCCCCCCCGGG")
+                .where("S", Predicates.controller(Predicates.blocks(definition.get())))
+                .where("X", Predicates.blocks("gtceu:robust_machine_casing")
+                    .or(Predicates.autoAbilities(definition.getRecipeTypes())))
+                .where("C", Predicates.blocks("gtceu:robust_machine_casing"))
+                .where("G", Predicates.blocks("gtceu:molybdenum_disilicide_coil_block"))
+                .where("A", Predicates.blocks("gtceu:tungstensteel_gearbox"))
+                .where("B", Predicates.blocks("gtceu:laminated_glass"))
+                .where("D", Predicates.blocks("gtceu:empty_tier_iii_battery"))
+                .where("E", Predicates.blocks("gtceu:heatproof_machine_casing"))
+                .where("#", Predicates.air())
+                .build())
+        .workableCasingRenderer("gtceu:block/casings/solid/machine_casing_robust_tungstensteel", "gtceu:block/multiblock/large_miner")
+
+    event.create("quantum_energy_decompressor", "multiblock") //量子能量解压机
+        .rotationState(RotationState.NON_Y_AXIS)
+        .allowExtendedFacing(false)
+        .generator(true)
+        .recipeType("quantum_energy_decompressor")
+        .tooltips(Component.translatable("gtceu.machine.perfect_oc"))
+        .tooltips(Component.translatable("gtceu.machine.available_recipe_map_1.tooltip",
+            Component.translatable("gtceu.quantum_energy_decompressor")))
+        .recipeModifier(GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.PERFECT_OVERCLOCK))
+        .appearanceBlock(GTBlocks.CASING_TUNGSTENSTEEL_ROBUST)
+        .pattern((definition) =>
+            FactoryBlockPattern.start()
+                .aisle("GGGGGCCCCCCCCCCCCGGGGG", "GGGGGXXXXXXXXXXXXGGGGG", "GGGGGCCCCCCCCCCCCGGGGG", "GGGGGCCCCCCCCCCCCGGGGG", "GGGGGCCCCCCCCCCCCGGGGG", "GGGGGCCCCCCCCCCCCGGGGG")
+                .aisle("GGGGGCCCCCCCCCCCCGGGGG", "BD##B############B##DB", "BD##B############B##DB", "BD##B############B##DB", "BD##B############B##DB", "GGGGGCCCCCCCCCCCCGGGGG")
+                .aisle("GGGGGCCCCCCCCCCCCGGGGG", "B#D#B############B#D#B", "B#D#B############B#D#B", "B#D#B############B#D#B", "B#D#B############B#D#B", "GGGGGCCCCCCCCCCCCGGGGG")
+                .aisle("GGGGGCCCCCCCCCCCCGGGGG", "B##DB############BD##B", "B##DBA#A#A#A#A#A#BD##B", "B##DB#A#A#A#A#A#ABD##B", "B##DB############BD##B", "GGGGGCCCCCCCCCCCCGGGGG")
+                .aisle("GGGGGCCCCCCCCCCCCGGGGG", "B###DAAAAAAAAAAAAD###B", "B###D############D###B", "B###D############D###B", "B###DAAAAAAAAAAAAD###B", "GGGGGCCCCCCCCCCCCGGGGG")
+                .aisle("GGGGGCCCCCCCCCCCCGGGGG", "B##DB############BD##B", "B##DBA#A#A#A#A#A#BD##B", "B##DB#A#A#A#A#A#ABD##B", "B##DB############BD##B", "GGGGGCCCCCCCCCCCCGGGGG")
+                .aisle("GGGGGCCCCCCCCCCCCGGGGG", "B#D#B############B#D#B", "B#D#B############B#D#B", "B#D#B############B#D#B", "B#D#B############B#D#B", "GGGGGCCCCCCCCCCCCGGGGG")
+                .aisle("GGGGGCCCCCCCCCCCCGGGGG", "BD##B############B##DB", "BD##B############B##DB", "BD##B############B##DB", "BD##B############B##DB", "GGGGGCCCCCCCCCCCCGGGGG")
+                .aisle("GGGGGCCCCCCCCCCCCGGGGG", "GGGGGCSCCCCCCCCCCGGGGG", "GGGGGCCCCCCCCCCCCGGGGG", "GGGGGCCCCCCCCCCCCGGGGG", "GGGGGCCCCCCCCCCCCGGGGG", "GGGGGCCCCCCCCCCCCGGGGG")
+                .where("S", Predicates.controller(Predicates.blocks(definition.get())))
+                .where("X", Predicates.blocks("gtceu:robust_machine_casing")
+                    .or(Predicates.autoAbilities(definition.getRecipeTypes())))
+                .where("C", Predicates.blocks("gtceu:robust_machine_casing"))
+                .where("G", Predicates.blocks("gtceu:molybdenum_disilicide_coil_block"))
+                .where("A", Predicates.blocks("gtceu:tungstensteel_gearbox"))
+                .where("B", Predicates.blocks("ae2:quartz_vibrant_glass"))
+                .where("D", Predicates.blocks("gtceu:empty_tier_iii_battery"))
+                .where("E", Predicates.blocks("gtceu:heatproof_machine_casing"))
+                .where("#", Predicates.air())
+                .build())
+        .workableCasingRenderer("gtceu:block/casings/solid/machine_casing_robust_tungstensteel", "gtceu:block/multiblock/large_miner")
 
 })
--- startup_scripts/recipe.js
+++ startup_scripts/recipe.js
@@ -8,4 +8,17 @@ GTCEuStartupEvents.registry("gtceu:recipe_type", event => {
         .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.MINER)
 
+    event.create("quantum_energy_compressor")
+        .setEUIO("in")
+        .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
+        .setMaxIOSize(4, 4, 1, 1)
+        .setProgressBar(GuiTextures.PROGRESS_BAR_WIREMILL, FillDirection.LEFT_TO_RIGHT)
+        .setSound(GTSoundEntries.COMPRESSOR)
+
+    event.create("quantum_energy_decompressor")
+        .setEUIO("out")
+        .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
+        .setMaxIOSize(2, 0, 0, 0)
+        .setProgressBar(GuiTextures.PROGRESS_BAR_WIREMILL, FillDirection.LEFT_TO_RIGHT)
+        .setSound(GTSoundEntries.CENTRIFUGE)
 })
