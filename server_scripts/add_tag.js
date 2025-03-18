4b87e1a2d78f8ccefe41fc2d3a0034923b061865
EypeZeo Preload <61043370+EypeZeo@users.noreply.github.com>
2025-03-18 15:32:06 +0800

    add gtceu tags for new processors

---
+++ server_scripts/add_tag.js
@@ -0,0 +1,6 @@
+ServerEvents.tags("item",event=>{
+    event.add("gtceu:circuits/uev",['mindx:uev_processor_mainframe']);
+    event.add("gtceu:circuits/uiv",['mindx:uiv_processor_mainframe']);
+    event.add("gtceu:circuits/uxv",['mindx:uxv_processor_mainframe']);
+    event.add("gtceu:circuits/opv",['mindx:opv_processor_mainframe']);
+})
